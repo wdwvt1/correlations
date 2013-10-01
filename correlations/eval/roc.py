3516ad2384e1b7475a90c004c249563d39478d52
wdwvt1 <wdwvt1@gmail.com>
2013-10-01 14:09:43 -0600

    Modified roc_edge_count.py and tests
    Modifications necessary to make sure that the ROC code would count edges properly in multiple events. Likely this code should replace the eco edge counter.

--- correlations/eval/roc.py
+++ correlations/eval/roc.py
@@ -10,30 +10,79 @@ __maintainer__ = "Will Van Treuren"
 __email__ = "wdwvt1@gmail.com"
 __status__ = "Development"
 
-from numpy import linspace, array
+from numpy import linspace, array, logical_xor
 import matplotlib.pyplot as plt
 from correlations.eval.result_eval import interacting_edges
 from biom.parse import parse_biom_table
 from correlations.eval.parse import SparCCResults
 
-def roc_counter(start, stop, dim, obs_edges):
-    '''Count the TP and FP based on observed edges and knowledge of true edges.
+def roc_edge_count(start, stop, lhs_dim, rhs_dim, obs_edges, true_edge_type):
+    '''Similar functionality to interacting_edges, but specifically for ROCs.
+
+    This code counts the number of edges that are true positives (TP), truly 
+    correlated (IT), and are positive (whether true positive or false positive, 
+    TT). 
 
     Inputs:
      start - int, the inclusive lower bound of the numerical part of the OTUs 
      that are correlated (e.g. 7 for o7). 
      stop - int, the exclusive upper bound of the numerical part of the OTUs 
      that are correalted (e.g. 7 for o7).
-     dim - int, number of OTUs on the LHS of the rule. 
+     lhs_dim - int, number of OTUs on the LHS of the rule. 
      obs_edges - list of tuples, observed OTU edges (e.g. [('o1','o19'),...]
+     true_edge_type - str, one of ['any','lhs_lhs','lhs_rhs','rhs_rhs']. 
+     determines what counts as true edges, i.e. LHS otu to LHS otu, LHS to
+     RHS (or RHS to LHS) otu, RHS to RHS otu, or any.
     '''
-    (total_detected, cis_edges, cis_cps, cis_mes, trans_edges, trans_cps,
-        trans_mes) = interacting_edges(start, stop, dim, obs_edges, 
-        interactions=['none']*len(obs_edges)) #interactions is hackish, but we don't
-    # need if for this function
-    TP = total_detected
+    if start/(lhs_dim+rhs_dim) != start//(lhs_dim+rhs_dim):
+        print('Start index is not evenly divisible by the total dimension.'+\
+            ' Normalizing to prevent errors.')
+        tmp_start = 0 #start - start 
+        tmp_stop = stop - start
+        def _f(x):
+            return (int(x[0][1:])-start, int(x[1][1:])-start)
+    else:
+        # evenly divisible means we don't need to normalize
+        def _f(x):
+            return (int(x[0][1:]), int(x[1][1:]))
+        tmp_start = start
+        tmp_stop = stop
+    TP = 0
+    int_edges = map(_f, obs_edges)
+    for edge in int_edges:
+        o1, o2 = edge
+        if tmp_start <= o1 < tmp_stop and tmp_start <= o2 < tmp_stop:
+            # calculate integer parts of o1,o2
+            i1 = o1//(lhs_dim+rhs_dim)
+            i2 = o2//(lhs_dim+rhs_dim)
+            if i1 == i2: #same relationship
+                r1 = o1%(lhs_dim+rhs_dim)
+                r2 = o2%(lhs_dim+rhs_dim)
+                if true_edge_type=='lhs_lhs': #both otus in LHS 
+                    if r1 < lhs_dim and r2 < lhs_dim: 
+                        TP+=1
+                elif true_edge_type=='rhs_rhs': #both otus in RHS 
+                    if r1 >= lhs_dim and r2 >= lhs_dim: 
+                        TP+=1
+                elif true_edge_type=='lhs_rhs': #one otu in LHS, one in RHS
+                    if logical_xor(r1 < lhs_dim, r2 < lhs_dim):
+                        TP+=1
+                elif true_edge_type=='any': #either of the above is allowed
+                    if any([r1 < lhs_dim and r2 < lhs_dim,
+                            r1 >= lhs_dim and r2 >= lhs_dim,
+                            logical_xor(r1 < lhs_dim, r2 < lhs_dim)]):
+                        TP+=1
+    # calculate the number of truly correlated things
+    groups = (stop-start)/float(lhs_dim+rhs_dim) #should be int, but jic.
+    if true_edge_type=='lhs_lhs':
+        IT = groups*lhs_dim*(lhs_dim-1)/2.
+    if true_edge_type=='rhs_rhs':
+        IT = groups*rhs_dim*(rhs_dim-1)/2.
+    if true_edge_type=='lhs_rhs':
+        IT = groups*lhs_dim*rhs_dim
+    if true_edge_type=='any':
+        IT = groups*(lhs_dim+rhs_dim)*(lhs_dim+rhs_dim-1)/2.
     TT = len(obs_edges)
-    IT = (stop-start)/(dim+1.)
     return TP, IT, TT
 
 def roc(TP, IT, TT, E):
--- correlations/tests/test_roc.py
+++ correlations/tests/test_roc.py
@@ -11,7 +11,7 @@ __maintainer__ = "Will Van Treuren"
 __email__ = "wdwvt1@gmail.com"
 __status__ = "Development"
 
-from correlations.eval.roc import (roc_counter, roc)
+from correlations.eval.roc import (roc_edge_count, roc)
 from cogent.util.unit_test import (TestCase, main)
 from numpy import array
 
@@ -22,106 +22,107 @@ class ROCTests(TestCase):
         '''Create variables needed for all tests.'''
         pass # nothing needed by all functions
 
-    def test_roc_counter(self):
+    def test_roc_ege_count(self):
         '''Test that TP, TT, IT are calculated correctly.'''
         # test basic 1d relationship of the form oX -> f(oX,oY)
         start = 0
         stop = 8
-        dim = 1
+        lhs_dim = 1
+        rhs_dim = 1
         obs_edges = [('o0', 'o7'), ('o1', 'o13'), ('o0', 'o3'), ('o1', 'o0'), 
             ('o0', 'o1'), ('o1', 'o3'), ('o2', 'o3')]
+        true_edge_type = 'any'
         exp_TP, exp_IT, exp_TT = 3, 4, 7
-        exp_TP, exp_IT, exp_TT = roc_counter(start, stop, dim, obs_edges)
+        obs_TP, obs_IT, obs_TT = roc_edge_count(start, stop, lhs_dim, rhs_dim,
+            obs_edges, true_edge_type)
         self.assertEqual(exp_TP, obs_TP)
         self.assertEqual(exp_IT, obs_IT)
         self.assertEqual(exp_TT, obs_TT)
         # test again with more complex relationship
         start = 60
         stop = 100
-        dim = 4
-        
+        lhs_dim = 4
+        rhs_dim = 1
+        obs_edges = [('o1','o65'), ('o64','o60'), ('o65','o64'), ('o71','o72'),
+            ('o80','o86'), ('o110','o111')]
+        true_edge_type = 'lhs_lhs'
+        exp_TP, exp_IT, exp_TT = 1, 48, 6
+        obs_TP, obs_IT, obs_TT = roc_edge_count(start, stop, lhs_dim, rhs_dim,
+            obs_edges, true_edge_type)
+        self.assertEqual(exp_TP, obs_TP)
+        self.assertEqual(exp_IT, obs_IT)
+        self.assertEqual(exp_TT, obs_TT)
+        # test cases that might give it trouble
+        start = 10
+        stop = 40
+        lhs_dim = 2
+        rhs_dim = 1
+        obs_edges = [('o10','o11'), ('o11','o12'), ('o10','o12'), ('o13','o12'),
+            ('o11','o13'), ('o13','o10')]
+        true_edge_type = 'lhs_rhs'
+        exp_TP, exp_IT, exp_TT = 2, 20, 6
+        obs_TP, obs_IT, obs_TT = roc_edge_count(start, stop, lhs_dim, rhs_dim,
+            obs_edges, true_edge_type)
+        self.assertEqual(exp_TP, obs_TP)
+        self.assertEqual(exp_IT, obs_IT)
+        self.assertEqual(exp_TT, obs_TT)
+        true_edge_type = 'rhs_rhs'
+        exp_TP, exp_IT, exp_TT = 0, 0, 6
+        obs_TP, obs_IT, obs_TT = roc_edge_count(start, stop, lhs_dim, rhs_dim,
+            obs_edges, true_edge_type)
+        self.assertEqual(exp_TP, obs_TP)
+        self.assertEqual(exp_IT, obs_IT)
+        self.assertEqual(exp_TT, obs_TT)
+        true_edge_type = 'lhs_lhs'
+        exp_TP, exp_IT, exp_TT = 1, 10, 6
+        obs_TP, obs_IT, obs_TT = roc_edge_count(start, stop, lhs_dim, rhs_dim,
+            obs_edges, true_edge_type)
+        self.assertEqual(exp_TP, obs_TP)
+        self.assertEqual(exp_IT, obs_IT)
+        self.assertEqual(exp_TT, obs_TT)
+        # test another strange case
+        start = 13
+        stop = 113
+        lhs_dim = 8
+        rhs_dim = 2
+        obs_edges = [('o10','o11'), ('o11','o12'), ('o10','o12'), ('o13','o12'),
+            ('o11','o13'), ('o13','o10'), ('o13', 'o20'), ('o14','o13'), 
+            ('o21','o22'),('o32','o31')]
+        true_edge_type = 'rhs_rhs'
+        exp_TP, exp_IT, exp_TT = 2, 10, 10
+        obs_TP, obs_IT, obs_TT = roc_edge_count(start, stop, lhs_dim, rhs_dim,
+            obs_edges, true_edge_type)
+        self.assertEqual(exp_TP, obs_TP)
+        self.assertEqual(exp_IT, obs_IT)
+        self.assertEqual(exp_TT, obs_TT)
+        true_edge_type = 'lhs_rhs'
+        exp_TP, exp_IT, exp_TT = 0, 160, 10
+        obs_TP, obs_IT, obs_TT = roc_edge_count(start, stop, lhs_dim, rhs_dim,
+            obs_edges, true_edge_type)
+        self.assertEqual(exp_TP, obs_TP)
+        self.assertEqual(exp_IT, obs_IT)
+        self.assertEqual(exp_TT, obs_TT)
+        true_edge_type = 'any'
+        exp_TP, exp_IT, exp_TT = 4, 450, 10
+        obs_TP, obs_IT, obs_TT = roc_edge_count(start, stop, lhs_dim, rhs_dim,
+            obs_edges, true_edge_type)
+        self.assertEqual(exp_TP, obs_TP)
+        self.assertEqual(exp_IT, obs_IT)
+        self.assertEqual(exp_TT, obs_TT)
 
+    def test_roc(self):
+        '''Test that the values needed for a ROC curve are calculated correctly.
+        '''
+        TP, IT, TT, E = 5., 10., 20., 100.
+        TN = E - (IT-TP+TT)
+        exp_sens, exp_spec = TP/IT, TN/(TT-TP+TN)
+        obs_sens, obs_spec = roc(TP, IT, TT, E)
+        self.assertEqual(exp_sens, obs_sens)
+        self.assertEqual(exp_spec, obs_spec)
 
-
-
-        true_positive_edges = [\
-        ('o0', 'o1'), ('o2', 'o3'), ('o4', 'o5'),
-        ('o6', 'o7'), ('o8', 'o9'), ('o10', 'o11'), 
-        ('o12', 'o13'), ('o14', 'o15'), ('o16', 'o17'), 
-        ('o18', 'o19'), ('o1', 'o0'), ('o3', 'o2'), 
-        ('o5', 'o4'), ('o7', 'o6'), ('o9', 'o8'), 
-        ('o11', 'o10'), ('o13', 'o12'), ('o15', 'o14'), 
-        ('o17', 'o16'), ('o19', 'o18')]
-        edges = cr.edges
-
-        true_stop = 20
-        total_otus = 100
-
-        totP = len(true_positive_edges)/2.
-        exp_totP = 10
-        unique_combinations = ((total_otus)*(total_otus-1))/2.
-        totN = unique_combinations
-        exp_unique_combinations = 4950
-        exp_edges = [\
-        ('o8', 'o14'), ('o8', 'o13'), ('o14', 'o9'),
-        ('o44', 'o61'), ('o13', 'o9'), ('o14', 'o13'),
-        ('o11', 'o14'), ('o11', 'o13'), ('o11', 'o9'),
-        ('o12', 'o19'), ('o11', 'o8'), ('o19', 'o18'),
-        ('o8', 'o9'), ('o83', 'o63')] 
-        TP=0.
-        FP=0.
-        for x in edges:
-            if x in true_positive_edges:
-                TP+=1
-            else:
-                FP+=1
-
-        FN = totP - TP
-        TN = totN - FP
-        sensitivity = TP/float(TP+FN)
-        specificity = TN/float(TN+FP) 
-        exp_TP = 2.0
-        exp_FN = 8.0
-        exp_FP = 12.0
-        exp_TN = 4938.0
-        exp_sensitivity = 0.2
-        exp_specificity = 0.997575757576
-
-
-        self.assertEqual(exp_totP, totP)
-        self.assertEqual(exp_unique_combinations, unique_combinations)
-        self.assertEqual(exp_edges, cr.edges)
-        self.assertEqual(exp_TP, TP)
-        self.assertEqual(exp_FN, FN)
-        self.assertEqual(exp_FP, FP)
-        self.assertEqual(exp_TN, TN)
-        self.assertEqual(exp_sensitivity, sensitivity)
-        self.assertEqual(exp_specificity, specificity)
 
 if __name__ == '__main__':
     main()
-
-
-
-
-CONET_LINES = [\
-'OTU1\tOTU2\tinteractionType\tmethodname_score\tp-value\tq-value\tsignificance\n',
-'o8\to14\tcopresence\t[sim_brownian=0.99, dist_kullbackleibler=0.079, correl_pearson=0.99, correl_spearman=0.99, dist_bray=0.09]\t0\t0\t100\n',
-'o8\to13\tcopresence\t[dist_kullbackleibler=0.079, dist_bray=0.13, sim_brownian=0.98, correl_spearman=0.999, correl_pearson=0.96]\t0\t0\t100\n',
-'o14\to9\tcopresence\t[dist_kullbackleibler=0.02, dist_bray=0.05, sim_brownian=0.997, correl_spearman=0.99, correl_pearson=0.996]\t0\t0\t100\n',
-'o44\to61\tcopresence\t[dist_kullbackleibler=1.87, dist_bray=0.5, sim_brownian=0.63, correl_spearman=0.45, correl_pearson=0.75]\t1.75E-5\t0.003675\t2.4347\n',
-'o13\to9\tcopresence\t[dist_kullbackleibler=0.035, dist_bray=0.068, sim_brownian=0.995, correl_spearman=-0.998, correl_pearson=0.995]\t0\t0\t100\n',
-'o14\to13\tcopresence\t[dist_kullbackleibler=0.11, dist_bray=0.117, sim_brownian=0.986, correl_spearman=0.988, correl_pearson=0.985]\t0\t0\t100\n',
-'o11\to14\tcopresence\t[dist_kullbackleibler=0.035, dist_bray=0.105, sim_brownian=0.988, correl_spearman=0.998, correl_pearson=0.987]\t0\t0\t100\n',
-'o11\to13\tcopresence\t[dist_kullbackleibler=0.001, dist_bray=0.013, sim_brownian=0.9998, correl_spearman=0.9998, correl_pearson=0.9998]\t0\t0\t100\n',
-'o11\to9\tcopresence\t[dist_kullbackleibler=0.022, dist_bray=0.055, sim_brownian=0.997, correl_spearman=-0.998, correl_pearson=0.997]\t0\t0\t100\n',
-'o12\to19\tcopresence\t[dist_kullbackleibler=0.126, dist_bray=0.09, sim_brownian=0.817, correl_spearman=0.948, correl_pearson=0.641]\t3.79E-6\t8.91E-4\t3.05\n',
-'o11\to8\tcopresence\t[dist_kullbackleibler=0.0738, dist_bray=0.122, sim_brownian=0.982, correl_spearman=0.999, correl_pearson=0.966]\t0\t0\t100\n',
-'o19\to18\tcopresence\t[dist_kullbackleibler=0.0146, dist_bray=0.022, sim_brownian=0.97, correl_spearman=-0.902, correl_pearson=0.992]\t4.37E-12\t1.59E-9\t8.798\n',
-'o8\to9\tcopresence\t[dist_kullbackleibler=0.058, dist_bray=0.102, sim_brownian=0.989, correl_spearman=0.998, correl_pearson=0.98]\t0\t0\t100\n',
-'o83\to63\tcopresence\t[dist_kullbackleibler=0.035, dist_bray=0.695, sim_brownian=0.514, correl_spearman=0.2807, correl_pearson=0.313]\t1.16E-5\t0.00256885\t2.59\n']
-
-
 
 
 
